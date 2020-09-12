f6e7b48cadc617578a8c62fb5691c1b96880097a
Jon Brouwer <jbrouw2@illinois.edu>
2020-09-12 17:16:18 -0500

    Birthday message

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Happy Birthday Mom!'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Birthday!'
 
 export default function Layout({ children, home }) {
   return (
